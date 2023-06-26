f5273a4f987c34f3b7cc9421c46096fb4787751a
euna0104 <koeuna0104@naver.com>
2023-06-27 04:59:59 +0900

    클래스형 컴포넌트로 구현

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }) {
-    return (
-    <div style={{ color }}>
-        { isSpecial && <b>*</b> } 
-        안녕하세요 {name}
-    </div>
-    )
+class Hello extends Component {
+    render() {
+        const {color, name, isSpecial } = this.props; //props 조회하기
+        return (
+            <div style={{ color }}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        );
+    }
 }
 
 Hello.defaultProps = {
-    name : '이름없음'
-}
+    name: '이름없음'
+};
 
 export default Hello;
