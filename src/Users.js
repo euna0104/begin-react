7184897f81b96ac3c0d954126bb035ac6455221a
euna0104 <koeuna0104@naver.com>
2023-06-29 14:18:34 +0900

    UsersContext 생성

--- src/Users.js
+++ src/Users.js
@@ -1,26 +1,20 @@
 import React, {useState} from 'react';
-import axios from 'axios';
-import useAsync from './useAsync';
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext';
 import User from './User';
-
-// useAsync 에서는 Promise의 결과를 바로 data에 담는다
-// 요청을 한 이후 response에서 data를 추출하여 반환하는 함수
-async function getUsers() {
-    const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/users`
-    );
-    return response.data;
-}
 
 function Users() {
     const [userId, setUserId] = useState(null);
-    const [state, refetch] = useAsync(getUsers, [], true);
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
 
-    const { loading, data: users, error } = state;
+    const { loading, data: users, error } = state.users;
+    const fetchData = () => {
+        getUsers(dispatch);
+    };
 
     if (loading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다</div>;
-    if (!users) return <button onClick={refetch}>불러오기</button>;
+    if (!users) return <button onClick={fetchData}>불러오기</button>;
     return (
         <>
             <ul>
@@ -34,7 +28,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId} />}
         </>
     );
