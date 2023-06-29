0e7a9abd42ec57694f4a1ccb49e32de28b469dee
euna0104 <koeuna0104@naver.com>
2023-06-29 23:55:03 +0900

    쿼리 받아오기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Route, Link, Routes } from "react-router-dom";
+import { Route, Link, Routes } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-      </ul>
-      <hr />
-      <Routes>
-        <Route path="/" exact={true} element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/about">소개</Link>
+          </li>
+        </ul>
+        <hr />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles/:username" component={Profile} />
+        </Routes>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <p>Home입니다.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Home입니다.</p>
+    </div>
+  );
 };
 
 export default Home;
