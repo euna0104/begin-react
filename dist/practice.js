d30d0d5df324a63c430aca1c4fc5be4febeffa21
euna0104 <koeuna0104@naver.com>
2023-07-24 17:40:51 +0900

    일반 객체를 interface로 타입 설정

--- dist/practice.js
+++ dist/practice.js
@@ -1,30 +1,10 @@
 "use strict";
-/* 인터페이스:
-인터페이스는 클래스 또는 객체를 위한 타입을 지정할 때 사용되는 문법이다 */
-var Circle = /** @class */ (function () {
-    function Circle(radius) {
-        this.radius = radius;
-    }
-    // 너비를 가져오는 함수 구현
-    Circle.prototype.getArea = function () {
-        return this.radius * this.radius * Math.PI;
-    };
-    return Circle;
-}());
-var Rectangle = /** @class */ (function () {
-    function Rectangle(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-    Rectangle.prototype.getArea = function () {
-        return this.width * this.height;
-    };
-    return Rectangle;
-}());
-var circle = new Circle(5);
-var rectangle = new Rectangle(10, 5);
-console.log(circle.radius);
-var shapes = [new Circle(5), new Rectangle(10, 5)];
-shapes.forEach(function (shape) {
-    console.log(shape.getArea());
-});
+var person = {
+    name: '홍길동',
+    age: 20
+};
+var expert = {
+    name: '박영희',
+    skills: ['javascript', 'react']
+};
+var people = [person, expert]; // 배열로 선언
--- src/practice.ts
+++ src/practice.ts
@@ -1,44 +1,20 @@
-/* 인터페이스:
-인터페이스는 클래스 또는 객체를 위한 타입을 지정할 때 사용되는 문법이다 */
-
-// Shape 이라는 interface 선언
-interface Shape {
-    getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자다.
+/* 일반 객체를 interface 로 타입 설정하기 */
+interface Person {
+    name: string;
+    age?: number; // 물음표가 들어갔다는 것은, 설정을 해도 되고 안해도 되는 값
+}
+interface Developer extends Person {
+    skills: string[];
 }
 
-class Circle implements Shape {
-    // implements 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시한다.
-    radius: number; // 멤버 변수 radius 값을 설정한다.
+const person: Person = {
+    name: '홍길동',
+    age: 20
+};
 
-    constructor(radius: number) {
-        this.radius = radius;
-    }
+const expert: Developer = {
+    name: '박영희',
+    skills: ['javascript', 'react']
+};
 
-    // 너비를 가져오는 함수 구현
-    getArea() {
-        return this.radius * this.radius * Math.PI;
-    }
-}
-
-class Rectangle implements Shape {
-    width: number;
-    height: number;
-    constructor(width: number, height: number) {
-        this.width = width;
-        this.height = height;
-    }
-    getArea() {
-        return this.width * this.height;
-    }
-}
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius);
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach(shape => {
-    console.log(shape.getArea());
-});
+const people: Person[] = [person, expert]; // 배열로 선언
