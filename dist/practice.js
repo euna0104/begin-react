9bc4196522e600f8eb3db012c9d431069bc21422
euna0104 <koeuna0104@naver.com>
2023-07-24 17:26:27 +0900

    클래스에서 interface 를 implements 하기

--- dist/practice.js
+++ dist/practice.js
@@ -1,7 +1,30 @@
 "use strict";
-/* 함수에서 타입 정의하기 */
-function sumArray(numbers) {
-    // 함수의 파라미터로 어떤 타입을 넣어야 하는지 바로 알 수 있다
-    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
-}
-var total = sumArray([1, 2, 3, 4, 5]);
+/* 인터페이스:
+인터페이스는 클래스 또는 객체를 위한 타입을 지정할 때 사용되는 문법이다 */
+var Circle = /** @class */ (function () {
+    function Circle(radius) {
+        this.radius = radius;
+    }
+    // 너비를 가져오는 함수 구현
+    Circle.prototype.getArea = function () {
+        return this.radius * this.radius * Math.PI;
+    };
+    return Circle;
+}());
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var circle = new Circle(5);
+var rectangle = new Rectangle(10, 5);
+console.log(circle.radius);
+var shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach(function (shape) {
+    console.log(shape.getArea());
+});
--- src/practice.ts
+++ src/practice.ts
@@ -1,7 +1,44 @@
-/* 함수에서 타입 정의하기 */
-function sumArray(numbers: number[]): number {
-    // 함수의 파라미터로 어떤 타입을 넣어야 하는지 바로 알 수 있다
-    return numbers.reduce((acc, current) => acc + current, 0);
+/* 인터페이스:
+인터페이스는 클래스 또는 객체를 위한 타입을 지정할 때 사용되는 문법이다 */
+
+// Shape 이라는 interface 선언
+interface Shape {
+    getArea(): number; // Shape interface 에는 getArea 라는 함수가 꼭 있어야 하며 해당 함수의 반환값은 숫자다.
 }
 
-const total = sumArray([1, 2, 3, 4, 5]);
+class Circle implements Shape {
+    // implements 키워드를 사용하여 해당 클래스가 Shape interface 의 조건을 충족하겠다는 것을 명시한다.
+    radius: number; // 멤버 변수 radius 값을 설정한다.
+
+    constructor(radius: number) {
+        this.radius = radius;
+    }
+
+    // 너비를 가져오는 함수 구현
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+
+class Rectangle implements Shape {
+    width: number;
+    height: number;
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius);
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach(shape => {
+    console.log(shape.getArea());
+});
