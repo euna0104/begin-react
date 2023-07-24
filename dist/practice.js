f8ca56059f5b4152e8ed4547e035b7a37db2ce57
euna0104 <koeuna0104@naver.com>
2023-07-24 16:20:53 +0900

    ts js compile practice

---
+++ dist/practice.js
@@ -0,0 +1,4 @@
+"use strict";
+var message = 'hello world'; // 상수 값이 문자열이라는 것 명시
+console.log(message);
+// ts 파일에서 명시한 값의 타입은 컴파일이 되는 과정에서 모두 사라지게 된다.
--- src/practice.ts
+++ src/practice.ts
@@ -1,19 +1,2 @@
-let count = 0; // num
-count += 1;
-count = '문자열' // error
-
-const message: string = 'hello world' // string
-
-const done: boolean = true; // boolean
-
-const numbers: number[] = [1, 2, 3]; // num array
-const messages: string[] = ['hello', 'world'] // string array
-
-messages.push(1); // error
-
-let mightBeUndefined: string | undefined = undefined; // string 일수도 있고 undefined 일수도 있음
-let nullableNumber: number | null = null; // number 일수도 있고 null 일 수도 있음
-
-let color: 'red' | 'yellow' | 'orange' = 'red'; // red, orange, yellow 중 하나임
-color = 'yellow';
-color = 'green'; // error
+const message: string = 'hello world'; // 상수 값이 문자열이라는 것 명시
+console.log(message);
