6ee2db06ed2f4b6f80610936e166db35ff015752
euna0104 <koeuna0104@naver.com>
2023-12-05 10:00:00 +0900

    update: input 상태 관리하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,44 +1,25 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 
 function InputSample() {
-  const [inputs, setInputs] = useState({
-    name: '',
-    nickname: ''
-  });
-
-  const nameInput = useRef();
-
-  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+  const [text, setText] = useState('');
 
   const onChange = (e) => {
-    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-    setInputs({
-      ...inputs, // 기존의 input 객체를 복사한 뒤
-      [name]: value // name 키를 가진 값을 value 로 설정
-    });
+    setText(e.target.value);
   };
 
   const onReset = () => {
-    setInputs({
-      name: '',
-      nickname: '',
-    });
-    nameInput.current.focus();
+    setText('');
   };
-
 
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
-      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
+      <input onChange={onChange} value={text} />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: </b>
-        {name} ({nickname})
+        <b>값: {text}</b>
       </div>
     </div>
   );
 }
 
 export default InputSample;
-{/*이렇게 하면 input 값을 입력한 다음 초기화를 누르면 input에 포커스가 잡힌다.*/}
