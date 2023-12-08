8702a153924763fec752473e4c90d2b09220d126
euna0104 <koeuna0104@naver.com>
2023-12-08 10:00:00 +0900

    update: 배열 렌더링하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,34 +1,32 @@
-import React, { useContext } from 'react';
-import { UserDispatch } from './App';
+import React from 'react';
 
-const User = React.memo(function User({ user }) {
-    const dispatch = useContext(UserDispatch);
+function User({ user }) {
   return (
     <div>
-      <b
-        style={{
-          cursor: 'pointer',
-          color: user.active ? 'green' : 'black'
-        }}
-        onClick={() => {
-            dispatch({ type: 'TOGGLE_USER', id: user.id});
-        }}
-      >
-        {user.username}
-      </b>
-      &nbsp;
-      <span>({user.email})</span>
-      <button 
-      onClick={() => {
-        dispatch({ type: 'REMOVE_USER', id: user.id});
-      }}
-      >
-        삭제</button>
+      <b>{user.username}</b> <span>({user.email})</span>
     </div>
   );
-});
+}
 
-function UserList({ users }) {
+function UserList() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
   return (
     <div>
       {users.map(user => (
@@ -38,4 +36,4 @@ function UserList({ users }) {
   );
 }
 
-export default React.memo(UserList);
+export default UserList;
