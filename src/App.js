e59bef438415b08476be4719ce8d7db1fa7460b8
euna0104 <koeuna0104@naver.com>
2023-12-12 10:00:00 +0900

    update: 배열 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -19,17 +19,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@gmail.com'
+      email: 'liz@gmail.com',
+      active: false
     }
   ]);
 
@@ -54,6 +57,14 @@ function App() {
     // =user.id 가 id 인 것을 제거한다.
     setUsers(users.filter(user => user.id !== id));
   };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? {...user, active: !user.active } : user
+      )
+    );
+  };
   return (
     <>
       <CreateUser
@@ -62,7 +73,7 @@ function App() {
          onChange={onChange}
          onCreate={onCreate}
        />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
