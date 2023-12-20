9c2a43213f01d5e7086233c410d6152ea713b42e
euna0104 <koeuna0104@naver.com>
2023-12-20 10:00:00 +0900

    update: 커스텀 Hooks 만들기

--- src/hooks/useInputs.js
+++ src/hooks/useInputs.js
@@ -1,31 +1,13 @@
-import { useReducer, useCallback } from 'react';
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'CHANGE':
-            return {
-                ...state,
-                [action.name]: action.value
-            };
-        case 'RESET':
-            return Object.keys(state).reduce((acc, current) => {
-                acc[current] = '';
-                return acc;
-            }, {});
-        default:
-            return state;
-    }
-}
+import { useState, useCallback } from 'react';
 
 function useInputs(initialForm) {
-    const [form, dispatch] = useReducer(reducer, initialForm);
-
-    const onChange = useCallback(e => {
-        const { name, value } = e.target;
-        dispatch({ type: 'CHANGE', name, value });
-    }, []);
-    const reset = useCallback(() => dispatch({ type: 'RESET' }), []);
-    return [form, onChange, reset];
+  const [form, setForm] = useState(initialForm);
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
+    setForm(form => ({ ...form, [name]:value }));
+  }, []);
+  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  return [form, onChange, reset];
 }
 
 export default useInputs;
