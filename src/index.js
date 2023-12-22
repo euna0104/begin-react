bc214a1a74b7118dec7e5face6ae1ac371eaebcd
euna0104 <koeuna0104@naver.com>
2023-12-22 10:00:00 +0900

    update: 클래스형 컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counter from './Counter';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Counter />
 );
 
 // If you want to start measuring performance in your app, pass a function
