56f0155997828d10940446a3789413699d036381
euna0104 <koeuna0104@naver.com>
2023-12-29 21:30:53 +0900

    feat: TodoHead 현재 할일 갯수

--- mashup-todolist/src/components/TodoHead.js
+++ mashup-todolist/src/components/TodoHead.js
@@ -29,12 +29,12 @@ const TodoHeadBlock = styled.div`
 
 function TodoHead() {
   const todos = useTodoState();
-  console.log(todos);
+  const undoneTasks = todos.filter(todo => !todo.done);
   return (
     <TodoHeadBlock>
       <h1>2023년 12월 28일</h1>
       <div className="day"> 수요일</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
   );
 }
